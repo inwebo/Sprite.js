026c903941b3f457660fc14a0d41cc1a561c52ab
inwebo <github@inwebo.net>
2020-05-24 18:27:25 +0200

    inherit from Render2d

--- src/RenderSprite/RenderSprite.js
+++ src/RenderSprite/RenderSprite.js
@@ -4,10 +4,6 @@ import { Renderer2D } from "@inwebo/render.js";
  * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/putImageData
  */
 export default class RenderSprite extends Renderer2D{
-    _clear(...subject) {
-        this.getCtx().clearRect(0,0, this._canvas.width, this._canvas.height);
-    }
-
     _draw(...subject) {
         /**
          * @type {Sprite}
