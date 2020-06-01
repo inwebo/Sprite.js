b2ef48666194160b2ee3b9482fa2f58f5e527517
inwebo <github@inwebo.net>
2020-06-01 21:42:38 +0200

    Update RenderSprite.js

--- src/RenderSprite/RenderSprite.js
+++ src/RenderSprite/RenderSprite.js
@@ -4,16 +4,11 @@ import { Renderer2D } from "@inwebo/render.js";
  * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/putImageData
  */
 export default class RenderSprite extends Renderer2D{
-    _draw(...subject) {
-        /**
-         * @type {Sprite}
-         */
-        const s = subject[0];
-
-        if(s.hasTransparencyColor()) {
-            s.applyTransparency();
+    _draw([sprite]) {
+        if(sprite.hasTransparencyColor()) {
+            sprite.applyTransparency();
         }
 
-        this.getCtx().putImageData(s.imgData,0,0);
+        this.getCtx().putImageData(sprite.imgData, 0, 0);
     }
 }
