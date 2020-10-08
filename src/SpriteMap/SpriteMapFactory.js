14f25dbd8e3aad6d6db589f1736a82a7fb6e5272
inwebo <github@inwebo.net>
2020-10-08 21:41:55 +0200

    Validate tiles entries

--- src/SpriteMap/SpriteMapFactory.js
+++ src/SpriteMap/SpriteMapFactory.js
@@ -1,3 +1,14 @@
+/**
+ * Validate a JSON input
+ */
 export default class SpriteMapFactory {
+    constructor(json) {
+        this._json = json;
+    }
 
+    validate() {
+        if(this._json.tiles === undefined) {
+            throw "Object's tile attribut is mandatory, see https://github.com/inwebo/Sprite.js/blob/master/docs/assets/img/mario.json";
+        }
+    }
 }
